68142c31384f96d1bcfc1fce81b27b4444b88d19
Felipenguim <felipe_teixeira@usp.br>
2026-06-11 09:47:03 -0300

    achando o pid

--- Exotic/arm/lab/lab01-cheat/code_arm.s
+++ Exotic/arm/lab/lab01-cheat/code_arm.s
@@ -107,31 +107,117 @@ START:
 	add x1, x1, #2 //type do dir/arquivo
 	ldrb w3, [x1]
 	cmp x3, #0x4 //dir type
+	mov x14, x2
 	b.ne .end_interaction //se não for diretório
 
-	sub x6, x2, #19 //total de bytes com o nome (len do nome)
+	sub x6, x14, #19 //total de bytes com o nome (len do nome)
 	add x1, x1, #1 //x1 endereço do nome agora
 
-	//fazer jeito de ver se o nome é composto por uma string de numeros 
-	
-
+	//fazer jeito de ver se o nome é composto por uma string de numeros (vai economizar um tempo mas é opcional)
+	// 	se byte < 0x30 → não é dígito → não é PID
+	// se byte > 0x39 → não é dígito → não é PID
+	//vou ver só a primeira letra para facilitar
+	ldrb w8, [x1]
+	cmp x8, #0x30
+	b.lt .end_interaction
+	cmp x8, #0x39
+	b.gt .end_interaction
 
 	//se for uma string de números aí sim vamos para a parte de olhar o nome do arquivo 
+	adr x8, pid_name_path //x8 vai ter o buffer de dir do nome 
+	adr x9, proc_dir_with_slash
+	mov x10, #0 //len /proc/
+	.loop_proc_str:
+		ldrb w11, [x9], #1   // lê byte de x9, depois x9 += 1
+		strb w11, [x8], #1   // escreve byte em x8, depois x8 += 1
+		add x10, x10, #1
+		cmp x10, #6
+		b.ne .loop_proc_str
+	
+	adr x8, pid_name_path+6 
+	mov x9, x1 //endereço do nome
+	//salvar o pid em x12 (vem como string)
+	adr x12, pid_number
+	mov x10, #0
+	.loop_name_pid_str:
+		ldrb w11, [x9], #1   // lê byte de x9, depois x9 += 1
+		cmp x11, #0 //ve se ta escrevendo um monte de padding ou não 
+		b.eq .end_loop_name_pid_str
+			strb w11, [x8], #1   // escreve byte em x8, depois x8 += 1
+			strb w11, [x12], #1
+			add x10, x10, #1
+			cmp x10, x6
+			b.ne .loop_name_pid_str
+		.end_loop_name_pid_str:
+
+	adr x8, pid_name_path+6 
+	add x8, x8, x10
+	adr x9, comm_dir
+	mov x10, #0
+	.loop_coom_str:
+		ldrb w11, [x9], #1
+		strb w11, [x8], #1
+		add x10, x10, #1
+		cmp x10, #5
+		b.ne .loop_coom_str
+
+	//pid_name_path com o path que quero 
+
+	mov x0, #-100
+	adr x1, pid_name_path
+	mov x2, #0x0 // O_RDONLY (0) 
+	mov x3, #0
+	_open
+	//fd in x0
+	adr x1, name_pid
+	mov x2, #16
+	_read
+
+	
+	//comparar se é o nome que eu quero ou não
+	//x1 tem o nome atual do pid
+	//x0 tem o len do nome com null terminator 
+	.loop_compare_names:
+		mov x13, x1 //endereço do nome 
+		adr x6, wanted_name
+		ldrb w7, [x13], #1
+		ldrb w8, [x6], #1
+		cmp x7, x8
+		b.ne .not_the_correct_pid
+		sub x0, x0, #1
+		cmp x0, #0
+		b.eq .correct_pid_finded
+
+
+	.correct_pid_finded:  //PAREI AQUI, mudar para fazer o que deve ser feito
+			adr x1, pid_number
+			mov x2, #16
+			mov x0, #1
+			bl print_chars
+			bl print_buffer_flush
+			b .done_cheat
+
+
+	//se não é o nome que eu quero, tem que limpar o buffer pid_name_path e ir para outro interação
+	.not_the_correct_pid:
+		//zerar o buffer para a próxima iteração 
+		adr x1, pid_name_path
+		stp xzr, xzr, [x1]
+		stp xzr, xzr, [x1, #16]
 
 
 	//ao terminar essa primeira intereção de achar o pid do tetris gerar uma função separada que vai ser o find_pid
+	//a pessoa deve mandar o nome com o pid e aí as coisas vão se resolver lá e o pid será retornado, ver se numero ou string
 
 
 .end_interaction:
-	sub x4, x4, x2
+	sub x4, x4, x14
 	cmp x4, #0
 	b.le .no_process_tetris
 
-	add x5, x5, x2
+	add x5, x5, x14
 	mov x1, x5 //novo endereço 
 	b .loop_buffer_proc
-
-	
 
 	
 .done_cheat:
@@ -147,6 +233,24 @@ START:
 
 proc_dir:
 	.asciz "/proc"
+
+proc_dir_with_slash:
+	.asciz "/proc/"
+
+comm_dir:
+	.asciz "/comm"
+
+pid_name_path:
+    .zero 32
+
+name_pid:
+	.zero 16
+
+wanted_name:
+	.asciz "dbus-daemon"
+
+pid_number:
+	.zero 8
 
 get_dir_info_buffer:
 	.zero BUFFER_SIZE_GETDENTS //4kb
